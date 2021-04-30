b7664a6076aa0b51b80e6962ce4d12bc74c2d48b
fedorax <fedorax@hotmail.co.jp>
2021-04-30 21:22:18 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function() {
+gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
-    .pipe(sass({outputStyle: 'expanded'}))
+    .pipe(sass({ outputStyle: 'expanded' }))
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('sass:watch', function() {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', gulp.series('sass'));
 });
